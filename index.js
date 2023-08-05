d413a974a344f189ee8c0b6060253ff3269088c7
Tosin Omotayo <oluwaetosin@gmail.com>
2023-08-05 06:22:56 +0100

    add docker dev set up

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 
+const redis = require('redis');
+
 const app = express();
 
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+
+client.set('visits', 0);
+
 app.get("/", (req, res)=>{
-    res.send("Hi there")
+
+    client.get('visits', (err,visits)=> {
+        res.send('Number of visits is '+ visits);
+        client.set('visits', parseInt(visits) + 1 );
+    });
+
+    res.send("Hi there");
 });
 
 app.listen(8080, ()=>{
